90478db766651b23c2ed9391f28b15ab2a68b367
Arthur <arthurclementte@hotmail.com>
2021-01-06 17:23:54 -0300

    Criadas duas novas features: números negativos e decimais.

--- main.js
+++ main.js
@@ -6,10 +6,38 @@ function testar(x, y){
     }
 }
 
-function gerar() {
-    let x = parseInt(document.getElementById("valorMinimo").value)
-    let y = parseInt(document.getElementById("valorMaximo").value)
+function checadoNeg(){
+    let caixaNeg = document.getElementById("checkNeg").checked
+    return caixaNeg
+}
 
+function checadoDec() {
+    let caixaDec = document.getElementById("checkDec").checked
+    return caixaDec
+}
+
+function eNegativo(num) {
+    if(num < 0){
+        return true
+    }else{
+        return false 
+    }
+}
+
+function gerarDec(x, y) {
+    if(x === y){
+        document.getElementById("saida").value = x
+    }
+    if(testar(x, y) === true){
+        let randomNum = Math.random() * (y - x) + x;
+        document.getElementById("saida").value = randomNum.toFixed(1)
+    }else{
+        alert('Intervalo inválido!!')
+        return
+    }
+}
+
+function gerarInt(x, y) {
     if(x === y){
         document.getElementById("saida").value = x
     }
@@ -19,5 +47,24 @@ function gerar() {
     }else{
         alert('Intervalo inválido!!')
         return
+    }
+}
+
+function main() {
+    let x = parseInt(document.getElementById("valorMinimo").value)
+    let y = parseInt(document.getElementById("valorMaximo").value)
+
+    if(checadoDec()){
+        if((eNegativo(x) || eNegativo(y)) && !checadoNeg()){
+            alert('Marque Negativos.')
+        }else{
+            gerarDec(x, y)
+        }
+    }else{
+        if((eNegativo(x) || eNegativo(y)) && !checadoNeg()){
+            alert('Marque Negativos.')
+        }else{
+            gerarInt(x, y)
+        }
     }
 }
